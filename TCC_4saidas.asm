4456c7e0297f44f099a7a6b7b86f5c8a9d8b3f6f
MarianoBW <marianoberwangerwille@gmail.com>
2018-06-28 14:55:27 -0300

    old

--- TCC_4saidas.asm
+++ TCC_4saidas.asm
@@ -7,7 +7,7 @@
 .def time3 = r19
 .def sum40 = r20
 .def dir   = r21
-.def flagint = r22
+
 .def flag40khz = R23
 .def pin40khz = r24
 
@@ -18,10 +18,7 @@
 
 .org 0x004           ; local da memoria do ext_int1 
 	rjmp int1_calc
-	
-.org 0x000E    ;local da memoria do TIM2_COMPA
-	rjmp TIM2_COMPA
-	
+
 .org 0x001A   ; timer1 ovf
 	rjmp timer1_ovf
 
@@ -46,7 +43,7 @@ start:
 	ldi temp,0b00001111 ; saidas para sinal de 40kHz, 1 para cada direcao
 	out DDRC,temp
 	
-	ldi temp,0b00000001 ;saida Direcao 3 (Oeste) + gravaçao e crystal
+	ldi temp,0b00000001 ;saida Direcao 4 (Oeste) + gravaçao e crystal
 	out DDRB,temp
 	
 	ldi temp,0b11100100 ;saida Direcao 3,2,1, (Sul,Leste,Norte, 00 ,max485 select Rx/Tx, 00)
@@ -65,74 +62,28 @@ start:
 	ldi temp, 0b00000110
 	sts UCSR0C, temp
 	
-;timer01 config
-	
-
-;bug repair atmega328p
-
-;	ldi temp,0b10000001 ; pausar timer 0 e 1
-;	out GTCCR,temp	
-;
-;	ldi temp,0b00000000
-;	out TCCR0A,temp
-;	out TCCR0B,temp
-;	ldi temp,0b00000010    ;configura TCC0A    CTC mode	
-;	out TCCR0A,temp
-;	ldi temp,0b00000001    ;configura TCC0B    clk/1 
-;	out TCCR0B,temp
-;	ldi temp,0b00000011    
-;	out OCR0A,temp
-;	ldi temp,0b00000000    
-;	sts TIMSK0,temp
-;	
-;	ldi temp,0b00000000 ; despausar timer 0 e 1
-;	out GTCCR,temp	
-;	nop
-;	nop
-;	nop
-
-
-
-
-	ldi temp,0b10000011 ; pausar timer 0, 1 e 2
-	out GTCCR,temp
-
-	;Timer2 40KHz
+	;Timer0 40KHz
 	ldi temp,0b00000000
-	sts TCCR2A,temp
-	sts TCCR2B,temp
+	out TCCR0A,temp
+	out TCCR0B,temp
 	ldi temp,0b00000010    ;configura TCC0A    CTC mode	
-	sts TCCR2A,temp
-	ldi temp,0b00000001    ;configura TCC0B    clk/1 
-	sts TCCR2B,temp
+	out TCCR0A,temp
+	ldi temp,0b00000000    ;configura TCC0B    clk=0 (ativa depois)
+	out TCCR0B,temp
 	ldi temp,0b11111000    ; OCR0A = 248  // para clk=20MHz
-	sts OCR2A,temp
-
-	ldi temp,0b00000010    ;liga clk 40khz
-	sts TIMSK2,temp
-
-	;Timer1 tempo de transito 
-	ldi temp,0b00000000
-	sts TCCR1A,temp        ;configura TCCR1A    normal mode
-	ldi temp,0b00000001    ;configura TCCR1B    clk/1
-	sts TCCR1B,temp
-	ldi temp,0b00000001    ;liga timer1 ovf
-	sts TIMSK1,temp
-
-	;Timer0 verificação de ultimo pulso
-	ldi temp,0b00000000
-	out TCCR0A,temp
-	out TCCR0B,temp
-	ldi temp,0b00000010    ;configura TCC0A    CTC mode	
-	out TCCR0A,temp
-	ldi temp,0b00000010    ;configura TCC0B    clk/8 
-	out TCCR0B,temp
-	ldi temp,0b00111111    ; OCR0A = 63  // para clk=20MHz
 	out OCR0A,temp
-
+	ldi temp,0b00000000    ; saida = 0
+	out PORTD,temp
 	ldi temp,0b00000010    ;liga clk 40khz
 	sts TIMSK0,temp
 
+	;Timer1 config
+	ldi temp,0b00000000
+	sts TCCR1A,temp        ;configura TCCR1A    normal mode
+	ldi temp,0b00000000    ;configura TCCR1B    clk=0 (ativa depois)
+	sts TCCR1B,temp
+	ldi temp,0b00000001    ;liga timer1 ovf
+	sts TIMSK1,temp
 
 	;interrupção externa
 	ldi temp,0b00001100 ;  The rising edge of INT1 generates an interrupt request 
@@ -150,19 +101,10 @@ start:
 	ldi dir,0b00000000
 	ldi flag40khz,0b00000000    ; flag 40khz
 	ldi pin40khz,0b00000000
-	
 	out PORTB,temp
 	out PORTC,temp
 	out PORTD,temp
-	
-	out TCNT0,temp		;|
-	sts TCNT1L,temp     ; -->zerar o tempo									
-	sts TCNT1H,temp		;|
-	; zerra controles de direção
-	cbi PORTD,5 ; entrada T1
-	cbi PORTD,6 ; entrada T2
-	cbi PORTD,7 ; entrada T3
-	cbi PORTB,0 ; entrada T4
+
 	; ativa interrupções
 	sei	
 	
@@ -185,25 +127,33 @@ RX:
 	ldi temp,0b00000000
 	sts UCSR0B, temp
 	
-	ldi sum40, 0b00000000 ;zera contador de pulsos
-	
 	; zera flag int1
 	ldi temp,0b00000010
 	sts EIFR, temp
-	cli
-	ldi flagint,0b00000000
+
 	; verifica se comando = 1
 	cpi dir, 0b00110001 
-	BREQ DIR1 ; se = 1
+	BRNE nao1 ; se /= 1
+	rjmp DIR1  ; se = 1
+
+nao1:
 	; verifica se comando = 2
 	cpi dir, 0b00110010 
-	BREQ DIR2
+	BRNE nao2 ; se /= 2 
+	rjmp DIR2  ; se = 2
+
+nao2:
 	; verifica se comando = 3
 	cpi dir, 0b00110011 
-	BREQ DIR3  ; se = 3
+	BRNE nao3 ; se /= 3 
+	rjmp DIR3  ; se = 3
+
+nao3:
 	; verifica se comando = 4
 	cpi dir, 0b00110100 
-	BREQ DIR4  ; se = 4
+	BRNE erro ; se /= 4 (comando invalido)
+	rjmp DIR2  ; se = 4
+
 erro:
 	ldi temp,0b00001000;TX on
 	sts UCSR0B, temp
@@ -217,13 +167,22 @@ erro:
 
 	rjmp comfim
 
+comfim: ; fim da comunicação
+	lds temp, UCSR0A 
+	sbrs temp,TXC0
+	rjmp comfim
+	ldi temp,0b01100000;TX fim
+	sts UCSR0A, temp
+	ldi temp,0b00010000; RX on
+	sts UCSR0B, temp
+	rjmp RX
 
 
 ;////////////////////////////////////// SELECAO SAIDA-ENTRADA ////////////////////////////////////
 DIR1:
 
-;	cli
-;	rjmp erro
+	cli
+	
 	ldi pin40khz,0b00000001  ; saida T1
 	
 	; ativa int1
@@ -234,28 +193,23 @@ DIR1:
 	out TCNT0,temp		;|
 	sts TCNT1L,temp     ; -->zerar o tempo									
 	sts TCNT1H,temp		;|
-	sts TCNT2,temp		;|
-	ldi time3,0b00000000
 
-;	ldi sum40, 0b00000000 ;zera contador de pulsos 
-	
-	ldi temp,0b10000001 ; despausar timer 2 inicio 40khz
-	out GTCCR,temp
-	
-	;ldi temp,0b00000001    
-	;out TCCR0B,temp	;configura TCCR0B    clk/1									         
-	;sts TCCR1B,temp ;configura TCCR1B    clk/1
+	ldi sum40, 0b00000000 ;zera contador de pulsos 
 
-;	cbi PORTD,5 ; entrada T4
-	sbi PORTD,6 ; entrada T3 on
-;	cbi PORTD,7 ; entrada T2
-;	cbi PORTB,0 ; entrada T1
+	ldi temp,0b00000001    
+	out TCCR0B,temp	;configura TCCR0B    clk/1									         
+	sts TCCR1B,temp ;configura TCCR1B    clk/1
+
+	cbi PORTD,5 ; entrada T1
+	cbi PORTD,6 ; entrada T2
+	sbi PORTD,7 ; entrada T3 on
+	cbi PORTB,0 ; entrada T4
 	sei
 	rjmp loop
 
 DIR2:
 
-;	cli
+	cli
 	
 	ldi pin40khz,0b00000010  ; saida T2
 	
@@ -268,28 +222,23 @@ DIR2:
 	out TCNT0,temp		;|
 	sts TCNT1L,temp     ; -->zerar o tempo									
 	sts TCNT1H,temp		;|
-	sts TCNT2,temp		;|
-	ldi time3,0b00000000
-	
-;	ldi sum40, 0b00000000 ;zera contador de pulsos 
-	
-	ldi temp,0b10000001 ; despausar timer 2 inicio 40khz
-	out GTCCR,temp
-	
-	;ldi temp,0b00000001    
-	;out TCCR0B,temp	;configura TCCR0B    clk/1									         
-	;sts TCCR1B,temp ;configura TCCR1B    clk/1
 
-	sbi PORTD,5 ; entrada T4 on
-;	cbi PORTD,6 ; entrada T3
-;	cbi PORTD,7 ; entrada T2
-;	cbi PORTB,0 ; entrada T1 
+	ldi sum40, 0b00000000 ;zera contador de pulsos 
+
+	ldi temp,0b00000001    
+	out TCCR0B,temp	;configura TCCR0B    clk/1									         
+	sts TCCR1B,temp ;configura TCCR1B    clk/1
+
+	cbi PORTD,5 ; entrada T1
+	cbi PORTD,6 ; entrada T2
+	cbi PORTD,7 ; entrada T3
+	sbi PORTB,0 ; entrada T4 on
 	sei
 	rjmp loop
 
 DIR3:
 	
-;	cli
+	cli
 	
 	ldi pin40khz,0b00000100  ; saida T3
 	
@@ -301,29 +250,24 @@ DIR3:
 	out TCNT0,temp		;|
 	sts TCNT1L,temp     ; -->zerar o tempo									
 	sts TCNT1H,temp		;|
-	sts TCNT2,temp		;|
-	ldi time3,0b00000000
 
-;	ldi sum40, 0b00000000 ;zera contador de pulsos 
+	ldi sum40, 0b00000000 ;zera contador de pulsos 
 
-	ldi temp,0b10000001 ; despausar timer 2 inicio 40khz
-	out GTCCR,temp
-	
-	;ldi temp,0b00000001    
-	;out TCCR0B,temp	;configura TCCR0B    clk/1									         
-	;sts TCCR1B,temp ;configura TCCR1B    clk/1
+	ldi temp,0b00000001    
+	out TCCR0B,temp	;configura TCCR0B    clk/1									         
+	sts TCCR1B,temp ;configura TCCR1B    clk/1
 
-;	cbi PORTD,5 ; entrada T4
-;	cbi PORTD,6 ; entrada T3
-;	cbi PORTD,7 ; entrada T2
-	sbi PORTB,0 ; entrada T1 on
+	sbi PORTD,5 ; entrada T1 on
+	cbi PORTD,6 ; entrada T2
+	cbi PORTD,7 ; entrada T3
+	cbi PORTB,0 ; entrada T4
 	sei
 	rjmp loop
 
 DIR4:
 
 	ldi pin40khz,0b00001000  ; saida T4
-;	cli
+	cli
 
 	; ativa int1
 	ldi temp,0b00000010 
@@ -333,177 +277,98 @@ DIR4:
 	out TCNT0,temp		;|
 	sts TCNT1L,temp     ; -->zerar o tempo									
 	sts TCNT1H,temp		;|
-	sts TCNT2,temp		;|
-	ldi time3,0b00000000
 
-;	ldi sum40, 0b00000000 ;zera contador de pulsos 
+	ldi sum40, 0b00000000 ;zera contador de pulsos 
 
-	ldi temp,0b10000001 ; despausar timer 2 inicio 40khz
-	out GTCCR,temp
-	
-	;ldi temp,0b00000001    
-	;out TCCR0B,temp	;configura TCCR0B    clk/1									         
-	;sts TCCR1B,temp ;configura TCCR1B    clk/1
+	ldi temp,0b00000001    
+	out TCCR0B,temp	;configura TCCR0B    clk/1									         
+	sts TCCR1B,temp ;configura TCCR1B    clk/1
 
-;	cbi PORTD,5 ; entrada T4
-;	cbi PORTD,6 ; entrada T3
-	sbi PORTD,7 ; entrada T2 on
-;	cbi PORTB,0 ; entrada T1
+	cbi PORTD,5 ; entrada T1
+	sbi PORTD,6 ; entrada T2 on
+	cbi PORTD,7 ; entrada T3
+	cbi PORTB,0 ; entrada T4
 	sei
 	rjmp loop
 
 ;////////////////////////////////////////// LOOP //////////////////////////////////////////////////
 
 loop:
-	sei
 	rjmp loop
 
 
 ;////////////////////////////////////////// 40kHz MAKE ////////////////////////////////////////////
-TIM2_COMPA:
+TIM0_COMPA:
 	cli
-	cpi sum40,0b00001111
-	breq fim40khz
-	
-	cpi flag40khz,0b00000000 ; compara flag com 0 
-	brne baixo ;se flag /= 0 vai para
-;	rjmp cima  ; seta 1 no pind6
-	;nop
-;cima:
-;sbi PORTD,6 ; seta 1 no pind6
+	;cpi sum40,0b00000101
+	;breq fim40khz
+	cpi flag40khz,0b00000000 ; se flag = 0 
+	brne baixo ;PC+2 ; entao   / pula 1 linha se nao igual
+	rjmp cima  ; seta 1 no pind6
+
+cima:
+	;sbi PORTD,6 ; seta 1 no pind6
 	out PORTC,pin40khz
 	ldi flag40khz,0b00000001 ; levanta flag 40khz
 	sei
-	INC sum40
-	;add sum40,flag40khz
+	add sum40,flag40khz
 	rjmp loop
 baixo:
-
 	;cbi PORTD,6 ; se nao, seta 0 no pind6
 	;out POTRC,
 	ldi flag40khz,0b00000000 ; zerra flag 40khz
-	out PORTC,flag40khz
 	sei
 	;RET
 	rjmp loop
 fim40khz:
+	cbi PORTD,6
 	ldi flag40khz,0b00000000
-	cpi pin40khz,0b00000000
-	out PORTC,pin40khz
-
-	ldi temp,0b10000010 ; despausar timer 0, 1 e pausa timer 2
-	out GTCCR,temp
-
-	rjmp loop
-
-int1_calc:
-	cli
-	ldi flagint,0b00000001
-	ldi temp,0b00000000 ; reseta timer 0
-	out TCNT0,temp		;|
 	
-	rjmp loop
-
-;////////////////////////////////////////////// AQUISICAO DE DADOS ///////////////////////////
-
-TIM0_COMPA:
-	cpi flagint,0b00000000
-	breq loop
-	ldi temp,0b10000011 ; pausar timer 0, 1 e 2
-	out GTCCR,temp
-	
-	lds time1,TCNT1L    ; guarda valores de tempo
-	lds time2,TCNT1H 
-
-	ldi temp,0b00000000 ;desativa int1
-	out EIMSK,temp
-	
-	;out PORTC,temp ;  seta 0 no pind2	
-	
-	; zerra controles de direção
-	cbi PORTD,5 ; entrada T1
-	cbi PORTD,6 ; entrada T2
-	cbi PORTD,7 ; entrada T3
-	cbi PORTB,0 ; entrada T4
-	
-;////////////////////////////////////////////// TRANSMICAO DOS DADOS //////////////////////////////////////////////////////	
-	out PORTC,temp ;  seta 0 no pind2
-	ldi temp,0b00001000;TX on
-	sts UCSR0B, temp
-	sbi PORTD,2 ; configura max485 para enviar dados	
+	ldi temp,0b00000000    ;configura TCC0B    clk off			 pg 107
+	sts TCCR0B,temp
 	sei
-	
-	;ldi time1,0b01000110
-	;ldi time2,0b01000111
-	;ldi time3,0b01001000
-	rjmp TXdir
-	;ldi temp,0b00000000    ;configura TCC0B    clk off			 pg 107
-	;sts TCCR0B,temp
-;	sei
 	;RET
-;	rjmp loop
+	rjmp loop
 
 ;/////////////////////////////////////////////// TIMER EXTRA //////////////////////////
 
 timer1_ovf:
-	;cli
+	cli
 	INC time3
 	sei
 	rjmp loop
 	
 ;////////////////////////////////////////////// AQUISICAO DE DADOS ///////////////////////////
 
-;int1_calc:
-;	cli
-;	ldi flagint,0b00000001
-;	cpi sum40,0b00000101
-;	brmi loop
-;	ldi temp,0b00000000 ; reseta timer 0
-;	out TCNT0,temp
-;	rjmp loop
-	
-	
-;	inc temp
-;	cpi temp, 0b00000001 
-;	BREQ loop ; se = 1
- 
-	
-;	ldi temp,0b10000001 ; pausar timer 0 e 1
-;	out GTCCR,temp
+int1_calc:
+	cli
 
-;	lds time1,TCNT1L    ; guarda valores de tempo
-;	lds time2,TCNT1H 
-	;ldi temp,0b00000000    
-	;sts TCCR1B,temp ;configura TCC1B    clk off	
-	;out TCCR0B,temp ;configura TCC0B    clk off	 
+	ldi temp,0b00000000    
+	sts TCCR1B,temp ;configura TCC1B    clk off	
+	out TCCR0B,temp ;configura TCC0B    clk off	 
 
-;;;;;;
-;	ldi temp,0b00000000 ;desativa int1
-;	out EIMSK,temp
-;;;;;;
-  
-	;inc time1
+	ldi temp,0b00000000 ;desativa int1
+	out EIMSK,temp
+
+	lds time1,TCNT1L    ; guarda valores de tempo
+	lds time2,TCNT1H    
 	
-;	out PORTC,temp ;  seta 0 no pind2
-;	ldi flag40khz,0b00000000 ; zerra flag 40khz
+	out PORTC,temp ;  seta 0 no pind2
+	ldi flag40khz,0b00000000 ; zerra flag 40khz
 	
 	; zerra controles de direção
-;	cbi PORTD,5 ; entrada T1
-;	cbi PORTD,6 ; entrada T2
-;	cbi PORTD,7 ; entrada T3
-;	cbi PORTB,0 ; entrada T4
+	cbi PORTD,5 ; entrada T1
+	cbi PORTD,6 ; entrada T2
+	cbi PORTD,7 ; entrada T3
+	cbi PORTB,0 ; entrada T4
 
 ;////////////////////////////////////////////// TRANSMICAO DOS DADOS //////////////////////////////////////////////////////	
 
-;	ldi temp,0b00001000;TX on
-;	sts UCSR0B, temp
-;	sbi PORTD,2 ; configura max485 para enviar dados	
-;	sei
-	
-	;ldi time1,0b01000110
-	;ldi time2,0b01000111
-	;ldi time3,0b01001000
-;	rjmp TXdir
+	ldi temp,0b00001000;TX on
+	sts UCSR0B, temp
+	sbi PORTD,2 ; configura max485 para enviar dados	
+	sei
+	rjmp TXdir
 
 ; comando de direcao 
 TXdir:
@@ -517,10 +382,7 @@ TXdirfim:
 	rjmp TXdirfim
 	ldi temp,0b01100000;TX fim
 	sts UCSR0A, temp
-	
-	
-	
-	;rjmp erro
+
 ; tempo 
 TXtime1:
 	lds temp, UCSR0A 
@@ -553,15 +415,3 @@ TXtime3:
     sts UDR0, time3
 
 	rjmp comfim
-	
-	
-comfim: ; fim da comunicação
-	lds temp, UCSR0A 
-	sbrs temp,TXC0
-	rjmp comfim
-	ldi temp,0b01100000;TX fim
-	sts UCSR0A, temp
-	ldi temp,0b00010000; RX on
-	sts UCSR0B, temp
-	rjmp RX
-
